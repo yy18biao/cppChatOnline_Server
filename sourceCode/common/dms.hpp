22d1bb9cb8189d55d6260e832b012dd1e64ca0e5
yy18biao <jingbiaoh@163.com>
2024-09-14 12:13:12 +0800

    封装短信发送sdk

---
+++ sourceCode/common/dms.hpp
@@ -0,0 +1,54 @@
+#pragma once
+#include <cstdlib>
+#include <iostream>
+#include <memory>
+#include <alibabacloud/core/AlibabaCloud.h>
+#include <alibabacloud/core/CommonRequest.h>
+#include <alibabacloud/core/CommonClient.h>
+#include <alibabacloud/core/CommonResponse.h>
+#include "log.hpp"
+
+class DMSClient
+{
+public:
+    using ptr = std::shared_ptr<DMSClient>;
+    DMSClient(const std::string &access_key_id,
+              const std::string &access_key_secret)
+    {
+        AlibabaCloud::InitializeSdk();
+        AlibabaCloud::ClientConfiguration configuration("cn-hangzhou");
+        configuration.setConnectTimeout(1500);
+        configuration.setReadTimeout(4000);
+        AlibabaCloud::Credentials credential(access_key_id, access_key_secret);
+        _client = std::make_unique<AlibabaCloud::CommonClient>(credential, configuration);
+    }
+
+    ~DMSClient() { AlibabaCloud::ShutdownSdk(); }
+
+    bool send(const std::string &phone, const std::string &code)
+    {
+        AlibabaCloud::CommonRequest request(AlibabaCloud::CommonRequest::RequestPattern::RpcPattern);
+        request.setHttpMethod(AlibabaCloud::HttpRequest::Method::Post);
+        request.setDomain("dysmsapi.aliyuncs.com");
+        request.setVersion("2017-05-25");
+        request.setQueryParameter("Action", "SendSms");
+        request.setQueryParameter("SignName", "聊天项目");
+        request.setQueryParameter("TemplateCode", "SMS_473160063");
+        request.setQueryParameter("PhoneNumbers", phone);
+        std::string param_code = "{\"code\":\"" + code + "\"}";
+        request.setQueryParameter("TemplateParam", param_code);
+        auto response = _client->commonResponse(request);
+        if (!response.isSuccess())
+        {
+            ERROR("短信验证码请求失败：{}", response.error().errorMessage());
+            return false;
+        }
+
+        std::cout << response.result().payload() << std::endl;
+
+        return true;
+    }
+
+private:
+    std::unique_ptr<AlibabaCloud::CommonClient> _client;
+};
